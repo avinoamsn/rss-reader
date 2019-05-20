c7118630b121e3e1e5619d6ba1ce715b2b9e9987
Avi Nerenberg <devavmeister@gmail.com>
2019-05-20 00:52:45 -0400

    cleaned up some additional files, added the build dir

---
+++ build/precache-manifest.05e9b5a6569e08e668517dee17a8e756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01273b8dccceea5fa62e9ed3d6567f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b4d58dbc499448e8b07",
+    "url": "/static/css/main.34aa7a1b.chunk.css"
+  },
+  {
+    "revision": "5f15ab10af7d6f027a86",
+    "url": "/static/js/2.9915928b.chunk.js"
+  },
+  {
+    "revision": "5b4d58dbc499448e8b07",
+    "url": "/static/js/main.dd9947f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
