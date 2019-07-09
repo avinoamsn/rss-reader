d2f4a52d9abf3646ccfb263d86b9a8c096b95194
Avi Nerenberg <devavmeister@gmail.com>
2019-07-09 19:41:22 -0400

    replaced Object.assign() w/ obj destructuring syntax in the reducer

--- src/redux/Reducers.tsx
+++ src/redux/Reducers.tsx
@@ -12,36 +12,40 @@ const defaultState = {
 
 /**
  * The root reducer.
- * 
+ *
  * @param state the state of the app
  * @param action the action being performed
  */
 const feed = (state = defaultState, action: any) => {
 	switch(action.type) {
 	 	case ACTIONS.Types.REQUEST_FEED:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				isFetching: true,
 				error: false,
 				errorMessage: '',
-			})
+			}
 		case ACTIONS.Types.RECIEVE_FEED:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				isFetching: false,
 				feed: action.payload,
 				renderedItems: [],
 				numRenderedItems: 0,
-			})
+			}
 		case ACTIONS.Types.ERROR:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				isFetching: false,
 				error: true,
 				errorMessage: action.payload,
-			})
+			}
 		case ACTIONS.Types.RENDER_ITEM:
-			return Object.assign({}, state, {
+			return {
+				...state,
 				renderedItems: state.renderedItems.concat(state.feed[state.numRenderedItems]),
 				numRenderedItems: state.numRenderedItems + 1,
-			})
+			}
 		default:
 			return state
 	}
